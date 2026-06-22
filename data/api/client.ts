4ec270d941bf433b7083d39d189b4788a022b75a
perzit0 <percy19marceliano@gmail.com>
2026-06-22 11:06:34 -0500

    feat: Vercel

--- data/api/client.ts
+++ data/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://backend-riesgo-crediticio.onrender.com';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
